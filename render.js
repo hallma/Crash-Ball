a51adade728ffe79c3946a5d0acd08227039b4c7
Lauren Docampo <laurendocampo@Laurens-MacBook-Pro.local>
2016-06-22 15:20:38 +0100

    Cleaner code + more powerful rendering

--- render.js
+++ render.js
@@ -1,71 +1,94 @@
 function platform(divId, frameWidth, frameHeight) {
 	var canvas = document.getElementById(divId);
+	canvas.width = frameWidth;
+	canvas.height = frameHeight;
 	var ctx = canvas.getContext("2d");
 	var rectWidth = 100;
 	var rectHeight = 15;
+	var ballR = 10;
+	var ballX = Math.random() * (frameWidth - ballR);
+	var ballY = Math.random() * (frameHeight - ballR);
+	var ballSpeedX = 3;
+	var ballSpeedY = 3;
+	var playerX = (frameWidth / 2) - (rectWidth / 2)
+	var playerY = 450;
+	var playerSpeedX = 3;
+	var playerSpeedY = 0;
 
-	canvas.width = frameWidth;
-	canvas.height = frameHeight;
-	bouncingBall(ctx, frameWidth, frameHeight, 10, 3, 3);
-	player(rectWidth, rectHeight, ctx, 500, 500, 1);
+	setInterval(function() {
+			ctx.clearRect(0, 0, frameWidth, frameHeight);
+			ballCoords = moveBall(ballX, ballY, ballSpeedX, ballSpeedY, frameWidth, frameHeight, ballR);
+			playerCoords = movePlayer(playerX, playerY, playerSpeedX, playerSpeedY, rectWidth, rectHeight, frameWidth, frameHeight);
+			drawBall(ctx, ballCoords['x'], ballCoords['y'], ballR);
+			ballX = ballCoords['x'];
+			ballY = ballCoords['y'];
+			ballSpeedX = ballCoords['speedX'];
+			ballSpeedY = ballCoords['speedY'];			
+			drawPlayer(ctx, playerCoords['x'], playerCoords['y'], rectWidth, rectHeight);
+			playerX = playerCoords['x'];
+			playerY = playerCoords['y'];
+			playerSpeedX = playerCoords['speedX'];
+			playerSpeedY = playerCoords['speedY'];
+	}, 30);
 }
 
-function bouncingBall(ctx, frameWidth, frameHeight, ballRadius, speedX, speedY) {
+// Moving functionality
+function moveBall(x, y, speedX, speedY, frameWidth, frameHeight, ballRadius) {
 	var r = ballRadius;
-	var x = Math.random() * (frameWidth - r);
-	var y = Math.random() * (frameHeight - r);
+	if (x <= r  ||  x >= (frameWidth - r)) {
+		speedX *= -1;		
+	} else if (y >= (frameHeight - r) || y <= r) {
+		speedY *= -1;
+	}
 
-	var ballSpawn = setInterval(function() {
-		ctx.clearRect(x - (r/1.1), y - (r/1.1), 3 * r, 3 * r);
-		x += speedX;
-		y += speedY;
-		if (x <= r  ||  x >= (frameWidth - r)) {
-			speedX *= -1;		
-		} else if (y >= (frameHeight - r) || y <= 10) {
-			speedY *= -1;
-		}
-		ball(x, y, r);
-	}, 1000.0 / 30);
+	x += speedX;
+	y += speedY;
 
-	function ball(x, y, r) {
-		ctx.beginPath();
-		ctx.arc(x, y, r, 0, 2*Math.PI);
-		ctx.fillStyle = "red";
-		ctx.fill();
-	    ctx.stroke();
+	return {
+		x: x,
+		y: y,
+		speedX: speedX,
+		speedY: speedY
 	};
 }
 
-function player(rectWidth, rectHeight, ctx, frameWidth, frameHeight,speed) {
-	var x = (frameWidth / 2) - (rectWidth / 2)
-	var y = 450;
-	var platformSpawn =  setInterval(function() {
-	    ctx.clearRect(x - (rectWidth / 2), y - (rectHeight / 2), rectWidth , rectHeight);
-		x += speed
+function movePlayer(x, y, speedX, speedY, rectWidth, rectHeight, frameWidth, frameHeight) {
+	document.addEventListener("keydown", function(event) {
+	  	if (event.which ==  37  && speedX > 0 ) {
+	  		speedX *= -1;
+	  	}
+	  	else if (event.which == 39 && speedX < 0) {
+	  		speedX *= -1;
+	  	}
+	});
 
-		document.addEventListener("keydown", function(event) {
-		  	if (event.which ==  37  && speed > 0 ) {
-		  		speed *= -1;
-		  	}
-		  	else if (event.which == 39 && speed < 0) {
-		  		speed *= -1;
-		  	}
+	if (x > (frameWidth - rectWidth) || x < 0) {
+		speedX *= -1;
+	};
 
-		});
-		if ( x > (frameWidth - rectWidth) || x < 0) {
-			speed *= -1;
-		}
+	x += speedX;
+	
+	return {
+		x: x,
+		y: y,
+		speedX: speedX,
+		speedY: speedY
+	};
+}
 
-		PlatformCanvas(x,y,ctx,rectWidth,rectHeight );
-	}, 1000.0 / 50);
-
+// Drawing functionality
+function drawBall(ctx, x, y, r) {
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, 2*Math.PI);
+	ctx.fillStyle = "red";
+	ctx.fill();
+	ctx.stroke();
 };
 
-function PlatformCanvas(x,y,ctx,width,height) {
+function drawPlayer(ctx, x, y, width, height) {
 	ctx.beginPath();
 	ctx.fillStyle="red";
-	ctx.fillRect(x,y,width,height); 
+	ctx.fillRect(x, y, width, height); 
 	ctx.stroke();
-
 };
 
